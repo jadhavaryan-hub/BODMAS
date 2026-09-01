947f02efb51b3140d92053567893ff466da511f1
jadhavaryan-hub <jadhavaryan2628@gmail.com>
2026-09-01 22:39:03 +0530

    Add files via upload

---
+++ Exercise_1_BODMAS.py
@@ -0,0 +1,47 @@
+#Addition
+x=28
+y=53
+z= 89
+print("Addition =",x + y)
+
+#Subtraction
+x = 23
+y = 28
+print("Subtraction=", x - y)
+
+#Multiplication
+x = 29
+y = 12
+print("Multiplicationn=",x*y)
+
+#True Division 
+x = 78
+y = 25
+print("True Division=",x/y)
+
+#Floor Division 
+x = 17
+y = 90  
+print("Floor Division=",x//y)
+
+#Modulus
+x = 76
+y = 82
+print("Modulus=", x % y)
+
+#Exponentitation
+x = 15
+y = 34
+print("Exponentitation=",x**y)
+
+#Combined Operations
+x = 34
+y = 49
+z= 89
+print("Combined Operations=", x + y * z - x / z )
+
+#Combined operations with parentheses
+x = 34
+y = 49
+z= 89
+print("Combined operations with parentheses=",(x + y) * z -( x / z) )
